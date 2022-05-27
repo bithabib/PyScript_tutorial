eedff33b0526bcc8d4299a0b595718f472cc7629
HabiburRahman1 <habib.usa2014@gmail.com>
2022-05-28 01:00:13 +0600

    update

--- python/database_handler.py
+++ python/database_handler.py
@@ -1,19 +1,22 @@
 import sqlite3
 from js import console
-conn = sqlite3.connect('test.db')
+
+conn = sqlite3.connect('database.db')
 
 def create(*args, **kwargs):
     conn.execute('''CREATE TABLE items
     (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    ietm_name           TEXT    NOT NULL,
+    item_name           TEXT    NOT NULL,
     quantity            INT     NOT NULL)''')
-    console.log("Creating database...")
+    console.log("Table created")
 
 def insert(*args, **kwargs):
-    conn.execute("INSERT INTO items (ietm_name, quantity) VALUES ('item1', 10)")
-    console.log("Inserting data...")
+    item = Element('item').element.value
+    quantity = Element('quantity').element.value
+    conn.execute("INSERT INTO items (item_name, quantity) VALUES ('{}', {})".format(item, quantity))
+    console.log("Item inserted")
 
 def fetch(*args, **kwargs):
-    cursor = conn.execute("SELECT id, ietm_name, quantity from items")
+    cursor = conn.execute("SELECT id, item_name, quantity FROM items")
     for row in cursor:
         console.log(row[0], row[1], row[2])
