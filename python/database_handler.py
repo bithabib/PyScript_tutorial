ae0d0205e1bf0750ddb5643204daac1e7362e626
HabiburRahman1 <habib.usa2014@gmail.com>
2022-05-27 15:46:04 +0600

    Database using pyscript

--- python/database_handler.py
+++ python/database_handler.py
@@ -2,5 +2,18 @@ import sqlite3
 from js import console
 conn = sqlite3.connect('test.db')
 
-def create():
+def create(*args, **kwargs):
+    conn.execute('''CREATE TABLE items
+    (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    ietm_name           TEXT    NOT NULL,
+    quantity            INT     NOT NULL)''')
     console.log("Creating database...")
+
+def insert(*args, **kwargs):
+    conn.execute("INSERT INTO items (ietm_name, quantity) VALUES ('item1', 10)")
+    console.log("Inserting data...")
+
+def fetch(*args, **kwargs):
+    cursor = conn.execute("SELECT id, ietm_name, quantity from items")
+    for row in cursor:
+        console.log(row[0], row[1], row[2])
